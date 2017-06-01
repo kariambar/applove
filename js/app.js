608442def92ec56801caeac4c5f4ae38d50644aa
karina palacios <karina.spalacios@gmail.com>
2017-05-31 21:40:12 -0400

    finalizando

--- js/app.js
+++ js/app.js
@@ -2,23 +2,18 @@
  * Archivo principal de funcionalidad de JS
  */
 
-
-
-
-
-
-(function modal(){
+(function modalDos(){
 	var boxes = Array.from(document.getElementsByClassName("col-4"));
 	var modal = document.getElementById("work-services-modal");
 	var modalBody = document.getElementsByClassName('modal-body')[0];
 	var close = document.getElementById("close");
 	boxes.forEach(function(box){
-		box.addEventListener("click", function(){
-			event.preventDefault();
-			modalBody.innerHTML = box.innerHTML;
-			modal.classList.remove("hidden");
-			close.addEventListener("click",function(){
-				modal.classList.add("hidden");
+    box.addEventListener("click", function(){
+    event.preventDefault();
+    modalBody.innerHTML = box.innerHTML;
+    modal.classList.remove("hidden");
+    close.addEventListener("click",function(){
+	modal.classList.add("hidden");
 			});
 		});		
 	});
